2c8aecf709d73cb411ec14249e3f78f60953dcf7
Hyunwoo Cho <barefoot1423@gmail.com>
2020-04-01 23:54:40 -0400

    Done Classes part

--- src/introduction/classes/Classes.kt
+++ src/introduction/classes/Classes.kt
@@ -2,3 +2,13 @@ package introduction.classes
 
 class Customer
 
+class Contact(val id: Int, var email: String)
+
+fun main() {
+    val customer = Customer()
+
+    val contact = Contact(1, "mary@gmail.com")
+
+    println(contact.id)
+    contact.email = "jane@gmail.com"
+}
