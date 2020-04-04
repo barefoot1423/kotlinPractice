e64d1c4f685bee4736c051147c5305e886cb58c5
Hyunwoo Cho <barefoot1423@gmail.com>
2020-04-03 22:47:21 -0400

    Done generic part

--- src/introduction/generic/Generics.kt
+++ src/introduction/generic/Generics.kt
@@ -1,3 +1,8 @@
 package introduction.generic
 
-class Generics
+fun <E> mutableStackOf(vararg  elements: E) = MutableStack(*elements)
+
+fun main() {
+    val stack = mutableStackOf(0.62, 3.14, 2.7)
+    println(stack)
+}
